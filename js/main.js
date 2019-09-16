d724b17802e892047d2b36177447c9449271a126
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2019-09-16 01:27:34 -0400

    updated index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-// JavaScript Document
+(() => {
+    
+    var button         =  	document.querySelector("#button"),
+        burgerCon      =  	document.querySelector("#burgerCon");
 
-console.log("Linked up");
+
+    function hamburgerMenu() {
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	function closeMenu() {
+
+		if (window.matchMedia("(min-width: 1024px)").matches && burgerCon.classList.contains('slideToggle') ) {
+		console.log("media query fired");
+		burgerCon.classList.remove('slideToggle');
+		button.classList.remove('expanded');
+		}
+    }
+    
+
+    button.addEventListener("click", hamburgerMenu, false);
+
+	window.addEventListener("resize", closeMenu);
+})();
